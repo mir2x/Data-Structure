25cc187a5ec6445c61ce34948cd02f7706131177
Tanim Ahmed <m_dev@tuta.io>
2023-01-02 04:43:02 +0600

    insertion at specific position and size of the list

--- linked-list/linked-list.c
+++ linked-list/linked-list.c
@@ -6,16 +6,27 @@ typedef struct Node {
     struct Node* next;
 } node_t;
 
-// add new elements at the beginning
+// size of the list
+int size(node_t *head) {
+    if(head == NULL) return 0;
+    int count = 0;
+    node_t *temp = head;
+    while(temp != NULL) {
+       count ++;
+       temp = temp -> next; 
+    }
+    return count;
+}
+
+// add new element at the beginning
 void unshift(struct Node **head_ref, int value) {
-	
 	node_t *new_node = (node_t *)malloc(sizeof(node_t));
 	new_node -> data = value;
 	new_node -> next = *head_ref;
 	*head_ref = new_node;
 }
 
-// add new elements at the end
+// add new element at the end
 void push(node_t **head_ref, int value) {
 
     node_t *new_node = (node_t *)malloc(sizeof(node_t));
@@ -34,6 +45,32 @@ void push(node_t **head_ref, int value) {
     temp -> next = new_node; 
 }
 
+// insert new element at the given position
+void insert(node_t **head_ref, int position, int value) {
+
+    if(position > size(*head_ref)) {
+        printf("Invalid position.\n");
+        return;
+    }
+        
+    if(position == 1) {
+        unshift(head_ref, value);
+        return;
+    }
+
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+	new_node -> data = value;
+	node_t *temp = *head_ref;
+    int i;
+    for(i = 0; i < position - 2; i++) {
+        temp = temp -> next;
+    }
+    new_node -> next = temp -> next;
+    temp -> next = new_node;
+}
+
+
+// print all elements of list
 void print(node_t *head) {
     if(head == NULL) printf("List is empty.");
     node_t *temp = head;
@@ -50,7 +87,8 @@ int main() {
     push(&head, 30); 
     push(&head, 40);
     unshift(&head, 0); 
+    insert(&head, 2, 5);
     print(head);
-    printf("\n");
+    printf("\nSize of the list : %d\n", size(head));
     return 0;
 }
